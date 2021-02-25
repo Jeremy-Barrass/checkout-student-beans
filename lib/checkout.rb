49172d65e03d73046c4e975f50f04814c4c2c554
Jeremy Barrass <jeremy.barrass@gmail.com>
2021-02-25 11:51:12 +0000

    begin refactor.

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -15,28 +15,7 @@ class Checkout
     total = 0
 
     basket.inject(Hash.new(0)) { |items, item| items[item] += 1; items }.each do |item, count|
-      if item == :apple || item == :pear
-        if (count >= 2)
-          leftover = count % 2
-          total += prices.fetch(item) * ((count - leftover) / 2)
-          total += prices.fetch(item) * leftover
-        else
-          total += prices.fetch(item) * count
-        end
-      elsif item == :banana || item == :pineapple
-        if item == :pineapple
-          total += (prices.fetch(item) / 2)
-          total += (prices.fetch(item)) * (count - 1)
-        else
-          total += (prices.fetch(item) / 2) * count
-        end
-      elsif item == :mango
-        if count >= 3
-         total += (prices.fetch(item)) * (count - 1)
-        end
-      else
-        total += prices.fetch(item) * count
-      end
+      
     end
 
     total
@@ -48,3 +27,26 @@ class Checkout
     @basket ||= Array.new
   end
 end
+
+      # if item == :apple || item == :pear
+      #   if (count >= 2)
+      #     leftover = count % 2
+      #     total += prices.fetch(item) * ((count - leftover) / 2)
+      #     total += prices.fetch(item) * leftover
+      #   else
+      #     total += prices.fetch(item) * count
+      #   end
+      # elsif item == :banana || item == :pineapple
+      #   if item == :pineapple
+      #     total += (prices.fetch(item) / 2)
+      #     total += (prices.fetch(item)) * (count - 1)
+      #   else
+      #     total += (prices.fetch(item) / 2) * count
+      #   end
+      # elsif item == :mango
+      #   if count >= 3
+      #    total += (prices.fetch(item)) * (count - 1)
+      #   end
+      # else
+      #   total += prices.fetch(item) * count
+      # end
